16b3d057ab67695c9398acba9bd1d32de7bbcd21
Unknown <jo14343@uvg.edu.gt>
2019-11-04 12:47:39 -0600

    Final para prueba

---
+++ fuzzy_logic.py
@@ -0,0 +1,70 @@
+import numpy as np
+from math import *
+
+def far(distance):
+    if (distance < 740 and distance > 370):
+        #print "far"
+        return (distance/370)-1
+    return 0
+
+def close(distance):
+    if (distance > 0 and distance < 370):
+        #print "close"
+        return -(distance/370)+1
+    return 0
+
+def mid(distance):
+    if (distance > 0 and distance < 370):
+        #print "mid1"
+        return distance/370
+    if (distance < 740 and distance > 370):
+        #print "mid2"
+        return -(distance/370)+2
+    return 0
+
+def right(angle):
+    if angle < 360 and angle > 180:
+        apt = 10*np.sin(np.deg2rad(angle))
+        #print "\nRIGHT APT\n", apt
+        return apt
+    return 0
+
+def left(angle):
+    if angle > 0 and angle < 180:
+        apt = -10*np.sin(np.deg2rad(angle))
+        #print "\nLEFT APT\n", apt
+        return apt
+    return 0
+
+def lost(angle):
+    if 90 < angle and angle < 270:
+        apt = 10*np.cos(np.deg2rad(angle))
+        #print "\nLOST APT\n", apt
+        return apt
+    else:
+        return 0
+
+def move_Horn(distance):
+    #if close and mid, then move is low
+    top_of_low_movement = min(close(distance), mid(distance))
+    #if mid, then move is mid
+    top_of_mid_movement = mid(distance)
+    #if mid, then move is mid
+    top_of_high_movement = min(far(distance), mid(distance))
+    #max movement will be 24 units per time, so there is 8 units per function
+    mpt = (8*(top_of_low_movement+top_of_mid_movement+top_of_high_movement))/24
+    #print "top_of_low_movement", top_of_low_movement
+    #print "top_of_mid_movement", top_of_mid_movement
+    #print "top_of_high_movement", top_of_high_movement
+    #print "Movement per time ", mpt
+    return 10*mpt
+
+
+def view_Horn(angle):
+    angle %= 360
+    #print "\nANGLE \n", angle
+    right_and_right = min(right(angle), left(angle))
+    lost_and_left = min(lost(angle), left(angle))
+    lost_and_right = min(lost(angle), right(angle))
+    apt = (120*(right_and_right+lost_and_left+lost_and_right))/360
+    return apt
